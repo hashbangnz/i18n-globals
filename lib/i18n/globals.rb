d326306bc5348742fa04d36988c6158f2c919d0d
Tim Robb <tim@robbmob.com>
2025-05-27 17:36:46 +1200

    Fix issues with using Thread.current with newer I18n versions

--- lib/i18n/globals.rb
+++ lib/i18n/globals.rb
@@ -4,15 +4,23 @@ require 'i18n/globals/version'
 module I18n
   class Config
     def globals
-      Thread.current["i18n_globals[#{Apartment::Tenant.current}]"] ||= {}
+      @@globals.presence || {}
     end
 
-    def globals=(globals)
-      Thread.current["i18n_globals[#{Apartment::Tenant.current}]"] = globals
+    def globals=(new_globals)
+      @@globals = new_globals
     end
   end
 
   class << self
+    def globals
+      config.globals
+    end
+
+    def globals=(new_globals)
+      config.globals = new_globals
+    end
+
     def translate(*args,**opts)
       opts = config.globals.merge(opts || {})
       super(*args,**opts)
